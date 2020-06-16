24f1c0ca583efe13344099b1d9824ba99b153cf4
Rebecca Ceppas <receppasdc@gmail.com>
2020-06-16 09:20:34 -0400

    fixed bounce method, need to figure out gravity direction

--- gravity.py
+++ gravity.py
@@ -4,6 +4,19 @@ import math
 
 background_colour = (255,255,255)
 (width, height) = (400, 400)
+drag = 0.999
+elasticity = 0.75
+gravity = (math.pi, 0.002)
+
+
+def add_vectors(v1, v2):
+    angle1, length1 = v1
+    angle2, length2 = v2 
+    x = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    y = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    length = math.hypot(x, y)
+    angle = math.pi/2 - math.atan2(y, x)
+    return (length, angle)
 
 class Particle():
 
@@ -19,24 +32,33 @@ class Particle():
         pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
 
     def move(self):
+        (self.angle, self.speed) = add_vectors((self.angle, self.speed), gravity)
         self.x += math.cos(self.angle) * self.speed
-        self.y -= math.cos(self.angle) * self.speed
+        self.y -= math.sin(self.angle) * self.speed
+        self.speed *= drag
 
     def bounce(self):
         
         if self.x > width - self.size:
             self.x = 2 * (width - self.size) - self.x
             self.angle = math.pi - self.angle
+            self.speed *= elasticity
         elif self.x < self.size:
             self.x = 2 * self.size - self.x
             self.angle = math.pi - self.angle
+            self.speed *= elasticity
+
         
         if self.y > height - self.size:
             self.y = 2 * (height - self.size) - self.y
             self.angle = - self.angle
+            self.speed *= elasticity
+
         elif self.y < self.size:
             self.y = 2 * self.size - self.y
             self.angle = - self.angle
+            self.speed *= elasticity
+
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Movement')
