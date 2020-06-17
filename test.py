e66f4d65d5a74f1945458e5a7c1631437e1ee36d
Rebecca Ceppas <receppasdc@gmail.com>
2020-06-16 20:33:53 -0400

    fixed gravity direction, added mouse interaction

--- test.py
+++ test.py
@@ -8,25 +8,18 @@ drag = 0.999
 elasticity = 0.75
 gravity = (math.pi, 0.002)
 
-
-def add_vectors(v1, v2):
+def addVectors(v1, v2):
     angle1, length1 = v1
-    angle2, length2 = v2 
-    x = math.sin(angle1) * length1 + math.sin(angle2) * length2
-    y = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    angle2, length2 = v2
+    x  = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    y  = math.cos(angle1) * length1 + math.cos(angle2) * length2
     
-    length = math.hypot(x, y)
-    angle = math.pi/2 - math.atan2(y, x)
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    length  = math.hypot(x, y)
 
     return (angle, length)
 
-def find_particle(particles, x, y):
-    for p in particles:
-        if math.hypot(p.x - x, p.y - y) <= p.size:
-            return p
-
 class Particle():
-
     def __init__(self, position, size):
         self.x, self.y = position
         self.size = size
@@ -34,79 +27,65 @@ class Particle():
         self.thickness = 1
         self.speed = 0
         self.angle = 0
-        
+
     def display(self):
         pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
 
     def move(self):
-        (self.angle, self.speed) = add_vectors((self.angle, self.speed), gravity)
+        (self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
         self.speed *= drag
 
     def bounce(self):
-        
         if self.x > width - self.size:
-            self.x = 2 * (width - self.size) - self.x
+            self.x = 2*(width - self.size) - self.x
             self.angle = - self.angle
             self.speed *= elasticity
+
         elif self.x < self.size:
-            self.x = 2 * self.size - self.x
+            self.x = 2*self.size - self.x
             self.angle = - self.angle
             self.speed *= elasticity
 
-        
         if self.y > height - self.size:
-            self.y = 2 * (height - self.size) - self.y
-            self.angle = math.pi - self.angle
-            self.speed *= elasticity
-        elif self.y < self.size:
-            self.y = 2 * self.size - self.y
+            self.y = 2*(height - self.size) - self.y
             self.angle = math.pi - self.angle
             self.speed *= elasticity
 
+        elif self.y < self.size:
+            self.y = 2*self.size - self.y
+            self.angle = math.pi - self.angle
+            self.speed *= elasticity
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Movement')
-
+pygame.display.set_caption('Tutorial 6')
 
 number_of_particles = 10
 my_particles = []
 
 for n in range(number_of_particles):
-    size = random.randint(10,20)
-    position = (random.randint(size, width - size), random.randint(size, height - size))
-    particle = Particle(position, size)
+    size = random.randint(10, 20)
+    x = random.randint(size, width-size)
+    y = random.randint(size, height-size)
+
+    particle = Particle((x, y), size)
     particle.speed = random.random()
-    particle.angle = random.uniform(0, math.pi * 2)
+    particle.angle = random.uniform(0, math.pi*2)
+
     my_particles.append(particle)
 
-selected_particle = None
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            (mouseX, mouseY) = pygame.mouse.get_pos()
-            selected_particle = find_particle(my_particles, mouseX, mouseY)
-        elif event.type == pygame.MOUSEBUTTONUP:
-            selected_particle = None
-
-    if selected_particle:
-        (mouseX, mouseY) = pygame.mouse.get_pos()
-        dx = mouseX - selected_particle.x
-        dy = mouseY - selected_particle.y
-        selected_particle.angle = math.pi/2  + math.atan2(dy, dx)
-        selected_particle.speed = math.hypot(dx, dy) * 0.1
 
     screen.fill(background_colour)
 
     for particle in my_particles:
-        if particle != selected_particle:
-            particle.move()
-            particle.bounce()
+        particle.move()
+        particle.bounce()
         particle.display()
-        
+
     pygame.display.flip()
